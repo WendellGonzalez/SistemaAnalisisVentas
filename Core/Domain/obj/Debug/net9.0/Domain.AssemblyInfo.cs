1c58af094703a334450ac5ff0ab1ba065f8b257d
Wendell Gonzalez <wendellagpb@gmail.com>
2025-12-09 15:56:56 -0400

    Ultimos cambios

--- Core/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Core/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d782d4fc4573136acf3b6bd2e5b3e73faec40c31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc6d6cbea2ee6ba5bd89063bc31a968114de3de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
